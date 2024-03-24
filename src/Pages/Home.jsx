85494c337e32d3142c3e5b4f27bfdeb8abc595c0
Airijko <airijkoblade@gmail.com>
2024-03-23 21:55:08 -0500

    adjusted home page positions

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,44 +17,46 @@ const Home = () => {
           className="w-[400px] h-[400px] opacity-75"
         />
       </div>
-      <div className="flex flex-col pl-12 w-[500px]">
-        <h1 className="font-quicksand uppercase whitespace-nowrap">
-          <span className="text text-gray-600">━ Introduction</span>
-        </h1>
-        <h2 className="flex flex-col">
-          <span className="font-code text-blue-100 text-2xl uppercase">
-            BIT Graduate 2024
-          </span>
-          <span className="font-quicksand text-yellow-500 font-bold text-3xl">
-            Software Developer
-          </span>
-        </h2>
-        <p className="flex flex-col text font-grotesk mt-2">
-          <span>
-            I'm a Business Information Technology Graduate at managing servers
-            and database, UI/UX design, and networking.
-          </span>
-        </p>
+      <div className="flex w-full justify-center">
+        <div className="w-[400px]">
+          <h1 className="font-quicksand uppercase whitespace-nowrap">
+            <span className="text text-gray-600">━ Introduction</span>
+          </h1>
+          <h2 className="flex flex-col">
+            <span className="font-code text-blue-100 text-2xl uppercase">
+              BIT Graduate 2024
+            </span>
+            <span className="font-quicksand text-yellow-500 font-bold text-3xl">
+              Software Developer
+            </span>
+          </h2>
+          <p className="flex flex-col text font-grotesk mt-2">
+            <span>
+              I'm a Business Information Technology Graduate at managing servers
+              and database, UI/UX design, and networking.
+            </span>
+          </p>
 
-        <div className="flex flex-row gap-5 mt-10">
-          <a href="https://www.linkedin.com/in/jonathan-jimena-jr/">
-            <SocialIcons name="linkedin" />{" "}
-          </a>
-          <a href="https://github.com/Airijko">
-            <SocialIcons name="github" />
-          </a>
-          <a href="https://www.youtube.com/@Airijko">
-            <SocialIcons name="youtube" />
-          </a>
-          <a href="https://www.twitch.tv/airijko">
-            <SocialIcons name="twitch" />
-          </a>
-          <a href="https://discord.com/invite/ra7EbEvM4K">
-            <SocialIcons name="discord" />
-          </a>
-          <a href="https://www.tiktok.com/@airijko">
-            <SocialIcons name="tiktok" />
-          </a>
+          <div className="flex flex-row gap-5 mt-10">
+            <a href="https://www.linkedin.com/in/jonathan-jimena-jr/">
+              <SocialIcons name="linkedin" />{" "}
+            </a>
+            <a href="https://github.com/Airijko">
+              <SocialIcons name="github" />
+            </a>
+            <a href="https://www.youtube.com/@Airijko">
+              <SocialIcons name="youtube" />
+            </a>
+            <a href="https://www.twitch.tv/airijko">
+              <SocialIcons name="twitch" />
+            </a>
+            <a href="https://discord.com/invite/ra7EbEvM4K">
+              <SocialIcons name="discord" />
+            </a>
+            <a href="https://www.tiktok.com/@airijko">
+              <SocialIcons name="tiktok" />
+            </a>
+          </div>
         </div>
       </div>
     </section>
