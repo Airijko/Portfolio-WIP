9d3cdd53f086573d6882651ab42174de6baccf92
Airijko <airijkoblade@gmail.com>
2024-03-25 00:31:27 -0500

    converted urls to hash

--- src/Components/MainNavbar.jsx
+++ src/Components/MainNavbar.jsx
@@ -17,7 +17,7 @@ const MainNavbar = () => {
             {navigation.map((item) => (
               <a
                 key={item.id}
-                href={item.url}
+                href={`#${item.url}`}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 }px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
